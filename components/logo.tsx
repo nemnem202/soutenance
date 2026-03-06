93f50c3c32bebc1e8d661ef1d86a64b5bf8d8860
nemnem202 <naim.elhabbas@gmail.com>
2026-03-06 06:37:27 +0100

    upscaled logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 export default function Logo() {
   return (
     <a href="/" className="cursor-pointer ">
-      <img src={logo} alt="Logo" className="h-8 w-35 hover:fill-primary" width={140} />
+      <img src={logo} alt="Logo" className=" w-50 hover:fill-primary" width={200} />
     </a>
   );
 }
