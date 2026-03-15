33fddce4981dd61b118bc831211eb51d6843fd89
nemnem202 <naim.elhabbas@gmail.com>
2026-03-15 06:58:43 +0100

    added types and chord dictionnary

---
+++ components/game/chord-carousel.tsx
@@ -0,0 +1,25 @@
+import { CHORDS_DICTIONNARY } from "@/config/chords-dictionnary";
+import { CarouselChord, ChordHarmony, Notes } from "@/types/midi";
+import { faker } from "@faker-js/faker";
+
+const HARMONIES = Object.keys(CHORDS_DICTIONNARY) as ChordHarmony[];
+
+export const CHORDS_PLACEHOLDER: CarouselChord[] = Array.from({ length: 10 }, (_, index) => {
+  const root = faker.helpers.arrayElement(Notes);
+  const harm = faker.helpers.arrayElement(HARMONIES);
+
+  const tickStart = index * 480;
+  const tickEnd = tickStart + faker.number.int({ min: 240, max: 960 });
+
+  return {
+    index,
+    root,
+    harm,
+    tickStart,
+    tickEnd,
+  };
+});
+
+export default function ChordCarousel() {
+  return <>{CHORDS_PLACEHOLDER[0]}</>;
+}
