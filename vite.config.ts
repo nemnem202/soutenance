56053399c79e4a69a4d79722c7f3400e8c3c72d7
nemnem202 <naim.elhabbas@gmail.com>
2026-03-31 01:10:35 +0200

    corrected vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import vike from "vike/plugin";
 import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [vike(), react(), tailwindcss()],
-
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": new URL("./", import.meta.url).pathname,
