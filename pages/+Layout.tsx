f6a2740fa5cb82ec6764fe0e5fd63699899f3038
nemnem202 <naim.elhabbas@gmail.com>
2026-03-31 23:04:47 +0200

    implemented toaster

--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -3,12 +3,14 @@ import { TooltipProvider } from "@/components/molecules/tooltip";
 import LanguagesProvider from "@/providers/language-provider";
 import ScreenSizeProvider from "@/providers/screen-size-provider";
 import SessionProvider from "@/providers/session-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <ScreenSizeProvider>
       <LanguagesProvider>
         <SessionProvider>
+          <Toaster />
           <TooltipProvider>{children}</TooltipProvider>
         </SessionProvider>
       </LanguagesProvider>
