3f21004c2dbfa39b60019ecde58aad8440f7493b
nemnem202 <naim.elhabbas@gmail.com>
2026-04-21 13:23:14 +0200

    fix: dashboard page style

--- pages/index/dashboard/+Page.tsx
+++ pages/index/dashboard/+Page.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { navigate } from "vike/client/router";
 import { useData } from "vike-react/useData";
-import ArrowElipsisTopMenu from "@/components/features/layout/arrow-elipsis-top-menu";
 import { MediumPlaylistWrapper } from "@/components/features/playlist/playlists-widgets";
 import SizeAdapter from "@/components/molecules/size-adapter";
 import Searchbar from "@/components/organisms/searchbar";
@@ -10,9 +9,13 @@ import useSession from "@/hooks/use-session";
 import type { UserDetailsDto } from "@/types/dtos/user";
 import type { Data } from "./+data";
 import Headline from "@/components/ui/headline";
-import { AccountMenuContent } from "@/components/features/auth/account-menu";
+import MobileHeader from "@/components/features/layout/mobile-header";
 
 export default function Page() {
+  return <SizeAdapter sm={<Mobile />} md={<Desktop />} />;
+}
+
+function Desktop() {
   const { currentAccount } = useData<Data>();
   const { instance } = useLanguage();
 
@@ -23,11 +26,8 @@ export default function Page() {
   const account = currentAccount.success ? currentAccount.data : null;
 
   if (!account) return null;
-
-  if (!account) return null;
   return (
     <div className="flex flex-col">
-      <SizeAdapter sm={<ArrowElipsisTopMenu menuContent={<AccountMenuContent />} />} />
       <Headline>{instance.getItem("dashboard")}</Headline>
       <section>
         <Content user={account} />
@@ -36,12 +36,30 @@ export default function Page() {
   );
 }
 
+function Mobile() {
+  const { currentAccount } = useData<Data>();
+  const { instance } = useLanguage();
+
+  useEffect(() => {
+    if (!currentAccount.success) navigate("/404");
+  }, [currentAccount.success]);
+
+  const account = currentAccount.success ? currentAccount.data : null;
+  if (!account) return null;
+  return (
+    <>
+      <MobileHeader title={instance.getItem("dashboard")} />
+      <Content user={account} />
+    </>
+  );
+}
+
 function Content({ user }: { user: UserDetailsDto }) {
   const { session } = useSession();
   const { instance } = useLanguage();
   return (
     <div className="w-full">
-      <div className="ml-auto max-w-116 my-9">
+      <div className="ml-auto max-w-116 mb-9 md:my-9">
         <Searchbar placeholder={instance.getItem("search")} />
       </div>
       <div className="flex  gap-x-auto gap-y-5 flex-wrap container">
