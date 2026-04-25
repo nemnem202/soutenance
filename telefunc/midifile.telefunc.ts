2d52863aa3713c96259264c8c08a4344b6e4ba97
nemnem202 <naim.elhabbas@gmail.com>
2026-04-25 02:46:09 +0200

    feat: midi file is fetched

--- telefunc/midifile.telefunc.ts
+++ telefunc/midifile.telefunc.ts
@@ -1,7 +1,7 @@
 import MidiController from "@/controllers/MidiController";
 import { handleAction } from "@/lib/response-handler";
 
-export default async function onMidiFile() {
+export default async function onMidiFile(exerciseId: number) {
   const controller = new MidiController();
   return handleAction("Generate a midi file", () => {
     return controller.getMidiFromChords();
