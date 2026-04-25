cbc1b9264358c1c8ce1707015b803e19e41fbbe3
nemnem202 <naim.elhabbas@gmail.com>
2026-04-25 04:06:11 +0200

    feat: midi file generation is working

--- controllers/MidiController.ts
+++ controllers/MidiController.ts
@@ -2,38 +2,66 @@ import { spawn } from "node:child_process";
 import { randomBytes } from "node:crypto";
 import { readFile, unlink } from "node:fs/promises";
 import { logger } from "@/lib/logger";
-import { type ServerResponse, Status } from "@/types/server-response";
+import type { ChordsGridSchema, ExerciseSchema, MeasureSchema } from "@/types/entities";
+import type { Cell } from "@/types/music";
+import { CHORDS_DICTIONNARY } from "@/config/chords-dictionary";
+import { Status, type ServerResponse } from "@/types/server-response";
+import { Controller, type ControllerDeps } from "./Controller";
+import GameRepository from "@/repositories/gameRepository";
 
-export default class MidiController {
-  public async getMidiFromChords(): Promise<ServerResponse<Buffer>> {
+type ExerciseWithForcedChordGrid = ExerciseSchema & { chordsGrid: ChordsGridSchema };
+
+export default class MidiController extends Controller<ControllerDeps> {
+  private repository = new GameRepository(this.deps.client);
+
+  public async getMidiFile(
+    exerciseId: number,
+    userId: number | null
+  ): Promise<ServerResponse<Buffer>> {
+    const request = await this.repository.findOne(exerciseId, userId);
+    if (!request.success) return request;
+    if (!request.data.chordsGrid) throw new Error("Execises does not have chord grid");
+    const file = await this.generateMidiFile(request.data as ExerciseWithForcedChordGrid);
+    return {
+      success: true,
+      status: Status.Ok,
+      data: file,
+    };
+  }
+
+  private async generateMidiFile(
+    exercise: ExerciseWithForcedChordGrid
+  ): Promise<Buffer<ArrayBufferLike>> {
     const start = Date.now();
-    const content = [
-      "Tempo 110",
-      "SwingMode On",
-      "Groove Ballad",
-      "SeqSize 4",
+    const content = this.convertExerciseToMmaContent(exercise);
+    logger.info("Content: ", content);
+    // const content = [
+    //   "Tempo 110",
+    //   "SwingMode On",
+    //   "Groove Ballad",
+    //   "SeqSize 4",
 
-      "Repeat",
-      "1 Am7",
-      "2 Dm7",
-      "3 G7",
-      "4 CM7",
-      "5 FM7",
-      "6 Bm7b5",
-      "7 E7b9",
-      "RepeatEnding 1",
-      "Groove BalladFill", // fill activé seulement au dernier passage
-      "8 Am7 / E7b9 /",
-      "Groove Ballad", // on remet le groove normal pour la suite
-      "RepeatEnd 2",
+    //   "Repeat",
+    //   "1 A(add#9)",
+    //   "2 DmM7(add9)",
+    //   "3 Gsus(add#9)",
+    //   "4 CM7",
+    //   "5 FM7",
+    //   "6 Bm7b5",
+    //   "7 E7b9",
+    //   "RepeatEnding 1",
+    //   "Groove BalladFill",
+    //   "8 Am7 / E7b9 /",
+    //   "Groove Ballad",
+    //   "RepeatEnd 2",
 
-      "Groove BalladEnd",
-      "9 Am7",
-      "z",
-    ].join("\n");
+    //   "Groove BalladEnd",
+    //   "9 Am7",
+    //   "z",
+    // ].join("\n");
     const buffer = await this.generateMidiBuffer(content);
-    logger.info(`Midi generated in ${Date.now() - start}ms`);
-    return { success: true, status: Status.Ok, data: buffer };
+    logger.success(`Midi generated in ${Date.now() - start}ms`);
+    return buffer;
   }
 
   private async generateMidiBuffer(content: string): Promise<Buffer> {
@@ -68,5 +96,61 @@ export default class MidiController {
 
       mma.on("error", (err) => reject(new Error(`Failed to start MMA: ${err.message}`)));
     });
+  }
+
+  private convertExerciseToMmaContent(exercise: ExerciseWithForcedChordGrid): string {
+    const tempo: string = this.getTempo(exercise);
+    const swing: string = this.getSwingMode(exercise);
+    const groove: string = this.getGroove(exercise);
+    const sections: string[] = this.getSections(exercise);
+    const end = this.getEnd();
+
+    const content = [tempo, swing, groove, ...sections, end].join("\n");
+
+    return content;
+  }
+
+  private getTempo(exercise: ExerciseWithForcedChordGrid) {
+    return `Tempo ${exercise.defaultConfig.bpm}`;
+  }
+
+  private getSwingMode(exercise: ExerciseWithForcedChordGrid) {
+    return `SwingMode On`;
+  }
+
+  private getGroove(exercise: ExerciseWithForcedChordGrid) {
+    return `Groove Ballad`;
+  }
+
+  private getSections(exercise: ExerciseWithForcedChordGrid): string[] {
+    const measures: MeasureSchema[] = exercise.chordsGrid.sections.flatMap((section) => [
+      ...section.commonMeasures,
+      ...section.voltas.flatMap((v) => v.measures),
+    ]);
+
+    return measures.sort((a, b) => a.index - b.index).map((measure) => this.getMeasure(measure));
+  }
+
+  private getMeasure(measure: MeasureSchema) {
+    const chordCells: Extract<Cell, { kind: "Chord" }>[] = measure.cells.filter(
+      (c) => c.kind === "Chord"
+    );
+    const values = chordCells.map((cell) =>
+      // `${cell.chord.content.note}${CHORDS_DICTIONNARY[cell.chord.content.modifier]?.mmaLabel ?? ""}`
+      {
+        if (cell.chord.content.note === "%") {
+          return `/`;
+        } else {
+          return `${cell.chord.content.note}${CHORDS_DICTIONNARY[cell.chord.content.modifier]?.mmaLabel ?? ""}`;
+        }
+      }
+    );
+    const returnValue = `${measure.index} ${values.join(" ")}`;
+    logger.info("Measure", returnValue);
+    return returnValue;
+  }
+
+  private getEnd() {
+    return "z";
   }
 }
