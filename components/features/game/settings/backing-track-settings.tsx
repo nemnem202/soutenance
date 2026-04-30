afedd9945494e18391ba9b901931eb1efa11570b
nemnem202 <naim.elhabbas@gmail.com>
2026-04-30 16:11:11 +0200

    feat: added volume change

--- components/features/game/settings/backing-track-settings.tsx
+++ components/features/game/settings/backing-track-settings.tsx
@@ -13,10 +13,13 @@ import { Label } from "@/components/ui/label";
 import { useLanguage } from "@/hooks/use-language";
 import { SidebarSlider } from "../game-assets";
 import { ParamsAccordion } from "../game-sidebar";
+import useGame from "@/hooks/use-game";
+import SoundEngine from "@/midi-editor/engines/sound-engine";
 
 export default function BackingTrackSettings() {
   const [backingTackActive, setBackingTrackActive] = useState(false);
   const { instance } = useLanguage();
+  const { midiState } = useGame();
   return (
     <ParamsAccordion title={<h3 className="title-3">{instance.getItem("backing_track")}</h3>}>
       <div className="flex gap-4 w-full">
@@ -39,18 +42,20 @@ export default function BackingTrackSettings() {
       <div
         className={`flex flex-col w-full gap-2 py-2 ${!backingTackActive && "text-muted-foreground"}`}
       >
-        <SidebarSlider defaultValue={50} disabled={!backingTackActive}>
-          <p className="paragraph w-15">{instance.getItem("piano")}</p>
-        </SidebarSlider>
-        <SidebarSlider defaultValue={50} disabled={!backingTackActive}>
-          <p className="paragraph w-15">{instance.getItem("guitar")}</p>
-        </SidebarSlider>
-        <SidebarSlider defaultValue={50} disabled={!backingTackActive}>
-          <p className="paragraph w-15">{instance.getItem("bass")}</p>
-        </SidebarSlider>
-        <SidebarSlider defaultValue={50} disabled={!backingTackActive}>
-          <p className="paragraph w-15">{instance.getItem("drums")}</p>
-        </SidebarSlider>
+        {midiState?.tracks.map((track) => (
+          <>
+            <SidebarSlider
+              defaultValue={100}
+              disabled={!backingTackActive}
+              onValueChange={(value) =>
+                SoundEngine.get()?.changeChannelVolume(track.channel, value)
+              }
+            >
+              <p className="paragraph w-15">{track.instrument}</p>
+            </SidebarSlider>
+          </>
+        ))}
+
         <div className="w-full flex items-center">
           <Label className="paragraph w-15" htmlFor="style-select">
             {instance.getItem("style")}
