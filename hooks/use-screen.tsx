d5e520c43fe4f36b5b81271504dc5d2e91c695cb
nemnem202 <naim.elhabbas@gmail.com>
2026-05-01 13:35:55 +0200

    feat: added screen orientation in screen size provider

--- hooks/use-screen.tsx
+++ hooks/use-screen.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ScreenSizeContext } from "@/providers/screen-size-provider";
 
 export default function useScreen() {
-  const size = useContext(ScreenSizeContext);
-  if (!size) throw new Error("useScreen must be called insite it's provider !");
-  return size;
+  const context = useContext(ScreenSizeContext);
+  if (!context) throw new Error("useScreen must be called insite it's provider !");
+  return context;
 }
--- providers/screen-size-provider.tsx
+++ providers/screen-size-provider.tsx
@@ -10,11 +10,24 @@ const BREAKPOINTS = {
 
 export type ScreenSizeType = keyof typeof BREAKPOINTS;
 
-export const ScreenSizeContext = createContext<ScreenSizeType | undefined>(undefined);
+export type ScreenOrientationType = "horizontal" | "vertical";
+
+export const ScreenSizeContext = createContext<
+  { size: ScreenSizeType; orientation: ScreenOrientationType } | undefined
+>(undefined);
 
 export default function ScreenSizeProvider({ children }: { children: ReactNode }) {
   const data = useData<{ screen: ScreenSizeType }>();
-  const { screen } = data;
+  const { screen: screenSize } = data;
+
+  const [size, setSize] = useState<ScreenSizeType>(() => {
+    if (typeof window === "undefined") return screenSize;
+    return getCurrentScreenSize();
+  });
+
+  const [orientation, setOrientation] = useState<ScreenOrientationType>(
+    size === "sm" ? "vertical" : "horizontal"
+  );
 
   function getCurrentScreenSize(): ScreenSizeType {
     const width = window.innerWidth;
@@ -30,20 +43,30 @@ export default function ScreenSizeProvider({ children }: { children: ReactNode }
     return "sm";
   }
 
-  const [size, setSize] = useState<ScreenSizeType>(() => {
-    if (typeof window === "undefined") return screen;
-    return getCurrentScreenSize();
-  });
-
   // biome-ignore lint/correctness/useExhaustiveDependencies: qsdqsd
   useEffect(() => {
     const handler = () => {
       setSize(getCurrentScreenSize());
     };
 
+    screen.orientation.addEventListener("change", () => {
+      switch (screen.orientation.type) {
+        case "landscape-primary":
+        case "landscape-secondary":
+          return setOrientation("horizontal");
+        case "portrait-primary":
+        case "portrait-secondary":
+          return setOrientation("vertical");
+      }
+    });
+
     window.addEventListener("resize", handler);
     return () => window.removeEventListener("resize", handler);
   }, []);
 
-  return <ScreenSizeContext.Provider value={size}>{children}</ScreenSizeContext.Provider>;
+  return (
+    <ScreenSizeContext.Provider value={{ size: screenSize, orientation }}>
+      {children}
+    </ScreenSizeContext.Provider>
+  );
 }
