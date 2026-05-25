18437433805a06844d07eb708fbad2a19687aef2
nemnem202 <naim.elhabbas@gmail.com>
2026-05-25 17:47:43 +0200

    feat: added new actions to control config

--- components/features/game/settings/general-settings.tsx
+++ components/features/game/settings/general-settings.tsx
@@ -9,10 +9,15 @@ import SizeAdapter from "@/components/molecules/size-adapter";
 
 export default function GeneralSettings() {
   const { instance } = useLanguage();
+  const { midiState, dispatch } = useGame();
   return (
     <ParamsAccordion title={<h3 className="title-3">{instance.getItem("general")}</h3>}>
       <div className="mb-2">
-        <SwitchParam checked={true} order="label-switch" setChecked={() => {}}>
+        <SwitchParam
+          checked={!!midiState?.config.countIn}
+          order="label-switch"
+          setChecked={() => {}}
+        >
           <p className="paragraph text-foreground">{instance.getItem("count_before_play")}</p>
         </SwitchParam>
       </div>
@@ -30,14 +35,14 @@ function DesktopInputsGroup() {
         <SmallInput
           label={instance.getItem("transpose")}
           type="number"
-          defaultValue={0}
+          defaultValue={midiState?.config.transposition}
           containerClassName="w-full"
           icon={<p className="paragraph-sm text-muted-foreground">sem:</p>}
         />
         <SmallInput
           label={instance.getItem("practice")}
           type="number"
-          defaultValue={0}
+          defaultValue={midiState?.config.transpositionPractice}
           containerClassName="w-full"
           icon={<p className="paragraph-sm text-muted-foreground">sem:</p>}
           tooltip={<p className="paragraph-sm">Increase the global trampose at each loop.</p>}
@@ -45,7 +50,7 @@ function DesktopInputsGroup() {
         <SmallInput
           label={instance.getItem("repeats")}
           type="number"
-          defaultValue={3}
+          defaultValue={midiState?.config.repeats}
           containerClassName="w-full"
           icon={<p className="paragraph-sm text-muted-foreground">x</p>}
         />
@@ -68,7 +73,14 @@ function DesktopInputsGroup() {
         <SmallInput
           label={instance.getItem("bpm_practice")}
           type="number"
-          defaultValue={0}
+          defaultValue={midiState?.config.bpmPractice}
+          onBlur={(e) => {
+            let value = parseInt(e.currentTarget.value, 10);
+            if (value < 30) value = 30;
+            if (value > 500) value = 500;
+            e.currentTarget.value = value.toString();
+            dispatch({ type: Action.SET_BPM, bpm: value });
+          }}
           containerClassName="w-full"
           icon={<p className="paragraph-sm text-muted-foreground">+</p>}
           tooltip={<p className="paragraph-sm ">Increase the Bpm at each loop.</p>}
@@ -84,14 +96,48 @@ function MobileInputsGroup() {
   if (midiState)
     return (
       <div className={`flex flex-col w-full py-2 gap-5`}>
-        <SidebarSlider defaultValue={[0]} axis="y" min={-11} max={11} step={1}>
-          <p className="paragraph w-15">{instance.getItem("transpose")}</p>
+        <SidebarSlider
+          onValueChange={(value) =>
+            dispatch({ type: Action.SET_TRANSPOSITION, transposition: value[0] })
+          }
+          defaultValue={[midiState.config.transposition]}
+          axis="y"
+          min={-11}
+          max={11}
+          step={1}
+        >
+          <div className="w-full flex gap-2">
+            <p className="paragraph">{instance.getItem("transpose")}:</p>
+            <p className="paragaph text-primary">{midiState.config.transposition}</p>
+          </div>
         </SidebarSlider>
-        <SidebarSlider defaultValue={[0]} axis="y" min={-11} max={11} step={1}>
-          <p className="paragraph w-15">{instance.getItem("practice")}</p>
+        <SidebarSlider
+          onValueChange={(value) =>
+            dispatch({ type: Action.SET_TRANSPOSITION_PRACTICE, transposition: value[0] })
+          }
+          defaultValue={[midiState.config.transpositionPractice]}
+          axis="y"
+          min={-11}
+          max={11}
+          step={1}
+        >
+          <div className="w-full flex gap-2">
+            <p className="paragraph">{instance.getItem("practice")}:</p>
+            <p className="paragaph text-primary">{midiState.config.transpositionPractice}</p>
+          </div>
         </SidebarSlider>
-        <SidebarSlider defaultValue={[1]} axis="y" min={1} max={100} step={1}>
-          <p className="paragraph w-15">{instance.getItem("repeats")}</p>
+        <SidebarSlider
+          onValueChange={(value) => dispatch({ type: Action.SET_REPEATS, repeats: value[0] })}
+          defaultValue={[midiState.config.repeats]}
+          axis="y"
+          min={1}
+          max={100}
+          step={1}
+        >
+          <div className="w-full flex gap-2">
+            <p className="paragraph">{instance.getItem("repeats")}:</p>
+            <p className="paragaph text-primary">{midiState.config.repeats}</p>
+          </div>
         </SidebarSlider>
         <SidebarSlider
           onValueChange={(value) => dispatch({ type: Action.SET_BPM, bpm: value[0] })}
@@ -106,8 +152,18 @@ function MobileInputsGroup() {
             <p className="paragaph text-primary">{midiState.config.bpm}</p>
           </div>
         </SidebarSlider>
-        <SidebarSlider defaultValue={[0]} axis="y" min={5} max={200} step={1}>
-          <p className="paragraph w-15">{instance.getItem("bpm_practice")}</p>
+        <SidebarSlider
+          onValueChange={(value) => dispatch({ type: Action.SET_BPM_PRACTICE, bpm: value[0] })}
+          defaultValue={[midiState.config.bpmPractice]}
+          axis="y"
+          min={5}
+          max={200}
+          step={1}
+        >
+          <div className="w-full flex gap-2">
+            <p className="paragraph">{instance.getItem("bpm_practice")}:</p>
+            <p className="paragaph text-primary">{midiState.config.bpmPractice}</p>
+          </div>
         </SidebarSlider>
       </div>
     );
