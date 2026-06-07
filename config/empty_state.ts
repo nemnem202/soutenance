425ecd5083a253054b46a1389dd2c567ba611f9a
nemnem202 <naim.elhabbas@gmail.com>
2026-06-07 13:39:12 +0200

    fix: sound was still playing in background

---
+++ config/empty_state.ts
@@ -0,0 +1,36 @@
+import { State } from "@/midi-editor/types/instance";
+
+const EMPTY_STATE: State = {
+  config: {
+    bpm: 120,
+    bpmPractice: 0,
+    countIn: false,
+    currentMeasureOverline: false,
+    displayGuitarDiagrams: false,
+    displayPianoDiagrams: false,
+    loop: null,
+    ppq: 0,
+    repeats: 0,
+    signature: [0, 0],
+    subdivision: [0, 0],
+    transposition: 0,
+    transpositionPractice: 0,
+  },
+  currentTrackId: 0,
+  display: {
+    zoomY: 0,
+  },
+  measuresStarts: new Map(),
+  queuedActions: new Set(),
+  rawMidiBuffer: new Uint8Array(),
+  tracks: [],
+  transport: {
+    currentMeasureIndex: 0,
+    playbackPosition: 0,
+    start: 0,
+    status: "paused",
+    totalDuration: 0,
+  },
+};
+
+export default EMPTY_STATE;
