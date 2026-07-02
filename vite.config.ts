5157c6377b94ce2e74cc56135d0838b349d9cef9
nemnem202 <naim.elhabbas@gmail.com>
2026-07-02 18:11:47 +0200

    feat: basic pitch is working fine

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     include: ["@tonejs/midi"],
   },
   ssr: {
+    noExternal: ["@tonejs/midi"],
     external: ["@tonejs/midi"],
   },
   assetsInclude: ["**/*.sf3", "**/*.mid", "**/*.json", "**/*.bin"],
