99ee20d4853a7a9be3fad504998ee6cfaf5e7a25
nemnem202 <naim.elhabbas@gmail.com>
2026-07-02 22:02:20 +0200

    removed fortest in seeding

--- seed/seed.ts
+++ seed/seed.ts
@@ -4,7 +4,7 @@ import convertAllPlaylists from "./functions";
 async function seed() {
   try {
     logger.info("Seeding ...");
-    await convertAllPlaylists("forTest");
+    await convertAllPlaylists();
     logger.success("Db is seeded.");
   } catch (err) {
     logger.error("Seed Error: ", err);
