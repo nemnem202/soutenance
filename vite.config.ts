ace9effd9494a67e707b383233577cb3cd78914a
nemnem202 <naim.elhabbas@gmail.com>
2026-07-02 17:51:28 +0200

    feat: added basic pitch

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,7 @@ export default defineConfig({
     include: ["@tonejs/midi"],
   },
   ssr: {
-    noExternal: ["@tonejs/midi"], // ❌ retire ça si tu l'avais
-    external: ["@tonejs/midi"], // ✅ remplace par ça
+    external: ["@tonejs/midi"],
   },
-  assetsInclude: ["**/*.sf3", "**/*.mid"],
+  assetsInclude: ["**/*.sf3", "**/*.mid", "**/*.json", "**/*.bin"],
 });
